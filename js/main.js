07fc79f7afd88af40b5130103931df318f0ae9da
Mohamed <alagamycom66@gmail.com>
2026-07-16 22:05:01 +0300

    Initial project upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+  link.addEventListener('click', function() {
+    document.querySelector('.navbar-nav .nav-link.active')?.classList.remove('active');
+    
+    this.classList.add('active');
+  });
+});
